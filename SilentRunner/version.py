e9d8830ea6d957daefddffe94e859655c86e67d0
tigrousad <tigrousad@gmail.com>
2026-08-02 22:45:29 +0100

    Update plugin name and author in version.py

--- SilentRunner/version.py
+++ SilentRunner/version.py
@@ -2,7 +2,7 @@
 SilentRunner – version constants.
 """
 
-PLUGIN_NAME = "SilentRunner"
+PLUGIN_NAME = "SilentRunner - by tigrousad"
 PLUGIN_VERSION = "1.0.0"
 PLUGIN_DESCRIPTION = "Silent background script manager"
-PLUGIN_AUTHORS = "SilentRunner Project"
+PLUGIN_AUTHORS = "tigrousad"
